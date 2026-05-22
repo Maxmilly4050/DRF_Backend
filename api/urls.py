af98964419e9c214ee8c7ae20ac84c12fa1bb363
Maxmilly4050 <maxsteel4050@gmail.com>
2026-05-22 14:04:13 +0300

    Add product ID to serializer and implement product detail view

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list, name='product-list'),
+    path('products/<int:id>/', views.product_by_id, name='product-by-id'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from api.serializers import ProductSerializer
 from api.models import Product
 from rest_framework.response import Response
@@ -8,4 +8,10 @@ from rest_framework.decorators import api_view
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_by_id(request, id):
+    product = get_object_or_404(Product, id=id)
+    serializer = ProductSerializer(product)
     return Response(serializer.data)
