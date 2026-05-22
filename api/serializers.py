3134e7e7302940ecca7796d8a983b3eb84f0b077
Maxmilly4050 <maxsteel4050@gmail.com>
2026-05-22 17:24:04 +0300

    Fix serializer and view to include Order model and correct serializer configuration

--- api/serializers.py
+++ api/serializers.py
@@ -5,14 +5,21 @@ from .models import Order, Product, OrderItem
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = (
-            'id',
-            'name',
-            'description',
-            'price',
-            'stock',
-        )
+        fields = ('id', 'name', 'description', 'price', 'stock',)
     def validate_price(self, value):
         if value <= 0:
             raise serializers.ValidationError("Price must be greater than zero.")
         return value 
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ('product', 'quantity')
+        
+    
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Order
+        fields = ('order_id','user', 'created_at', 'status',)
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -14,4 +14,10 @@ def product_list(request):
 def product_by_id(request, id):
     product = get_object_or_404(Product, id=id)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
