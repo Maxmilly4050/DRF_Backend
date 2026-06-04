2f01b7b2028ba2999285631036d5e7c7eba37440
Maxmilly4050 <maxsteel4050@gmail.com>
2026-06-04 22:11:59 +0300

    Update `ProductDetailAPIView` to use `lookup_url_kwarg` and adjust product detail URL pattern

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view(), name='product-list'),
-    path('products/<int:pk>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
     path('orders/', views.order_list, name='order_list'),
     path('product_info/', views.product_info, name='product_info'),
     path('silk/', include('silk.urls', namespace='silk'))
--- api/views.py
+++ api/views.py
@@ -14,6 +14,7 @@ class ProductListAPIView(generics.ListAPIView):
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
 
 @api_view(['GET'])
 def order_list(request):
